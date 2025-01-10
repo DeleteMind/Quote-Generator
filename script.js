38cfecdc707638c5fcaffe9dd3e985963cdc8272
DeleteMind <filipe.santos.cordeiro@gmail.com>
2025-01-10 01:56:56 +0000

    refresh

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const  newQuteBtn= document.getElementById("new-quote");
 
+let apiQuotes = [];
+
+// Show New Quote
 function newQuote() {
+    // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     console.log(quote);
+    
 }
 
 
