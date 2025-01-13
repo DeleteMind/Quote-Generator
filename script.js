6ee85cdd185bcd5ff9f5f0022a418aeb8ee94be6
DeleteMind <filipe.santos.cordeiro@gmail.com>
2025-01-13 14:25:42 +0000

    loading_complete

--- script.js
+++ script.js
@@ -5,11 +5,26 @@ const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const  newQuteBtn= document.getElementById("new-quote");
 const threadsBtn = document.getElementById('threads')
+const loader = document.getElementById('loader')
 
 let apiQuotes = [];
 
+// Show Loading
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank an replace with 'Unknown'
@@ -25,10 +40,12 @@ function newQuote() {
         quoteText.classList.remove('long-quote');
     }
     quoteText.textContent = quote.quote;
+    complete();
 }
 
 
 async function getQuote() {
+    loading();
     const apiUlr = 'https://gist.githubusercontent.com/awran5/355643af99164a61ae0f95c84206d151/raw/c62636e8eef7e73540fa04b67f753ca9b95ee21e/quotes-api.js';
     try {
         const response = await fetch(apiUlr);
@@ -55,4 +72,5 @@ function threadsQuote() {
 twitterBtn.addEventListener('click', tweetQuote);
 newQuteBtn.addEventListener('click', newQuote);
 threadsBtn.addEventListener('click', threadsQuote)
+
 getQuote();
